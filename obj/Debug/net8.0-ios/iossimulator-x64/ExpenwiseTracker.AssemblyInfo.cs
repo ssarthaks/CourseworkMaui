a24b22e00b30f02eb3a31619e12015868bdac242
ssarthaks <ssarthakxd@gmail.com>
2025-01-15 19:12:41 +0545

    Background added to login page

--- obj/Debug/net8.0-ios/iossimulator-x64/ExpenwiseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/ExpenwiseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenwiseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d1d97fefa401e19edf70fbbfe11f14b3d3993c13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e60d023597ccf43289c39693547cf725b87398e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenwiseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenwiseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ExpenwiseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ExpenwiseTracker.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenwiseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d8c53fdc7ae3e7037b7a52edcfbf8815d4ccbdc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e60d023597ccf43289c39693547cf725b87398e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenwiseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenwiseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
